1f668636e1e3f108bed8edd4144097f7007f8412
lgcholoquinga <ldu-aries.lk@hotmail.es>
2020-02-11 14:14:00 -0500

    proyecto final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const celeste = document.getElementById('celeste');
 const violeta = document.getElementById('violeta');
 const naranja = document.getElementById('naranja');
 const verde = document.getElementById('verde');
-const ULTIMO_NIVEL = 1;
+const ULTIMO_NIVEL = 5;
 
 class Juego {
     constructor(){
